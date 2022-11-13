5dc67fbf87eb966d35b3b57143460b9d7e12381b
tnahs <31777797+tnahs@users.noreply.github.com>
2022-11-12 16:35:57 -0800

    update: update Apple Books archive/restore scripts

--- scripts/restore-apple-books-library.sh
+++ scripts/restore-apple-books-library.sh
@@ -1,7 +1,29 @@
-#!/bin/zsh
+#!/usr/bin/env zsh
 
 
-TEMPDIR=$(mktemp -d 2> /dev/null)
+function print_help {
+    cat <<EOF
+Restore a previously archived Apple Books library
+
+Usage: restore-library.sh [PATH]
+
+Arguments:
+    PATH    Path to restore archive from
+
+Options:
+    -h, --help    Print help information
+EOF
+}
+
+
+function print_usage {
+    cat <<EOF
+
+Usage: restore-library.sh [PATH]
+
+For more information try '--help'.
+EOF
+}
 
 
 function quit_applebooks {
@@ -10,57 +32,61 @@ function quit_applebooks {
 }
 
 
-function delete_library {
-    echo "Deleting Apple Books library..."
+function delete_old_library {
+    echo "Deleting old Apple Books library..."
     rm -rf $HOME/Library/Containers/com.apple.BK*
     rm -rf $HOME/Library/Containers/com.apple.iBooks*
     rm -rf $HOME/Library/Group Containers/group.com.apple.iBooks
 }
 
 
+
+# Additional rsync options:
+#     --progress    prints per-file transfer progress
+#     --stats       prints file transfer stats on completion
 function restore_library {
+    # The trailing forward-slash here is important. It tells 'rsync' to move the
+    # archive directory's *contents* into the target. Otherwise it would
+    # move the archive *directory* into the target.
+    local archive="$1"/
 
-    # The trailing slash here is important. It tells 'rsync' to move the
-    # *contents* of this directory into another. Otherwise it would move the
-    # directory as a whole.
-    archive="$1"/
+    echo "Restoring archived Apple Books library..."
+    echo "This may take a few minutes..."
 
-    echo "Restoring Apple Books library..."
     rsync \
-        --verbose \
-        --progress \
-        --archive \
-        "$archive" \
-        $HOME/Library/Containers/
-}
-
-
-# For reference.
-function archive_applebooks {
-    rsync \
-        --verbose \
-        --progress \
         --archive \
         --extended-attributes \
-        $HOME/Library/Containers/com.apple.BK* \
-        $HOME/Library/Containers/com.apple.iBooks*
-        [DESTINATION]
+        "$archive"/Containers \
+        $HOME/Library/Containers/
+
+    rsync \
+        --archive \
+        --extended-attributes \
+        "$archive"/Group\ Containers \
+        $HOME/Library/Group\ Containers/
 }
 
 
 function main {
-    if [[ $# -ne 1 ]] then;
-        echo "Error: Please provide a single path to an Apple Books library archive."
+    if [[ $1 == "--help" ||  $1 == "-h" ]] then;
+        print_help
+        exit 1
+    elif [[ $# -lt 1 ]] then;
+        echo "error: Missing required positional argument: PATH"
+        print_usage
+        exit 2
+    elif [[ $# -gt 1 ]] then;
+        echo "error: Invalid or missing arguments"
+        print_usage
         exit 2
     else
         quit_applebooks
-        delete_library
+        delete_old_library
         restore_library "$1"
-        echo "Apple Books library restored!"
+        echo "Restore complete!"
         echo "Please restart before running Apple Books."
     fi
 }
 
 
 main "$@"
-

c810e3cdb7223f81272187a670dc4d54407e13de
tnahs <31777797+tnahs@users.noreply.github.com>
2022-11-12 16:30:38 -0800

    update: remove unused python packages

--- scripts/install-pipx-packages.sh
+++ scripts/install-pipx-packages.sh
@@ -6,13 +6,7 @@ function install_pipx_packages {
     printf "\nInstalling pipx packages...\n\n"
 
     pipx_packages=(
-        "flake8"
-        "black"
-        "mypy"
-        "isort"
         "bpython"
-        "youtube-dl"
-        "gallery-dl"
     )
 
     for package in $pipx_packages; do

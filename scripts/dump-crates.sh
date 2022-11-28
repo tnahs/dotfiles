8a5a58b89e9005ff1448160346671c556239485e
Shant Ergenian <shaaant@pm.me>
2022-11-27 20:34:51 -0800

    feat: add cargo dump and install scripts

---
+++ scripts/dump-crates.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env zsh
+
+
+CRATES_JSON=$HOME/.cargo/.crates2.json
+CRATES_TXT=$HOME/.dotfiles/cargo/crates.txt   
+
+
+# Dump crates installed from crates.io.
+function dump_crates {
+
+		# Note how the "cargo-edit" entry contains the string "registry+https://"
+		# whereas the one with "helix-term" contains "path+file:///". This string is
+		# used below in the jq command to filter out any crates that are installed
+		# from a local build of the application.
+		#
+    # {
+    #   "installs": {
+    #     "cargo-edit 0.11.3 (registry+https://github.com/rust-lang/crates.io-index)": {
+    #         ...
+    #     },
+    #     "helix-term 0.6.0 (path+file:///path/to/helix/helix-term)": {
+    #         ...
+    #     },
+    #     ...
+    #   }
+    # }
+    jq_command='.installs | keys[]  | select( . | contains("registry+https://")) | split(" ")[0]'
+
+		# Dump the results from the above command into a text file.
+    cat $CRATES_JSON | jq -r $jq_command > $CRATES_TXT
+}
+
+
+function main {
+		dump_crates
+}
+
+
+main
---
+++ scripts/install-crates.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env zsh
+
+
+CRATES_TXT=$HOME/.dotfiles/cargo/crates.txt   
+
+
+# Installs crates listed in a text file.
+function install_crates {
+		while read crate; do
+				cargo install $crate
+		done < $CRATES_TXT
+}
+
+
+function main {
+		install_crates
+}
+
+
+main

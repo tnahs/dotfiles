7e23ad0d96fb3c4915532021e8649a9cb586e0f0
Shant Ergenian <shaaant@pm.me>
2024-01-17 21:40:06 -0800

    feat: add "capture to clipboard" hotkeys

--- hammerspoon/config/screenshots.lua
+++ hammerspoon/config/screenshots.lua
@@ -1,5 +1,10 @@
 -- Screenshots ----------------------------------------------------------------
 
+-- screencapture
+--  -i    Capture screen interactively, by selection or window
+--  -W    Start interaction in window selection mode
+--  -o    In window capture mode, do not capture the shadow of the window
+--  -c    Force screen capture to go to the clipboard
 local command = "screencapture"
 local path = "$HOME/Downloads/screenshot-$(date +%Y-%m-%d-%H%M%S).png"
 
@@ -9,24 +14,38 @@ H:bind({}, "5", function()
     H.triggered = true
 end)
 
--- Capture selection
--- -i    Capture screen interactively, by selection or window
-H:bind({}, "7", function()
-    hs.execute(command .. " -i " .. path)
+-- Capture window to clipboard
+H:bind({}, "6", function()
+    hs.execute(command .. " -iWc " .. path)
     H.triggered = true
 end)
 
 -- Capture window
--- -i    Capture screen interactively, by selection or window
--- -W    Start interaction in window selection mode
--- -o    In window capture mode, do not capture the shadow of the window
-H:bind({}, "6", function()
+H:bind({ "option" }, "6", function()
     hs.execute(command .. " -iW " .. path)
     H.triggered = true
 end)
 
--- Capture full screen
+-- Capture selection to clipboard
+H:bind({}, "7", function()
+    hs.execute(command .. " -ic " .. path)
+    H.triggered = true
+end)
+
+-- Capture selection
+H:bind({ "option" }, "7", function()
+    hs.execute(command .. " -i " .. path)
+    H.triggered = true
+end)
+
+-- Capture full screen to clipboard
 H:bind({}, "8", function()
+    hs.execute(command .. " -c " .. path)
+    H.triggered = true
+end)
+
+-- Capture full screen
+H:bind({ "option" }, "8", function()
     hs.execute(command .. path)
     H.triggered = true
 end)

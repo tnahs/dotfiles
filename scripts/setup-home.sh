2aa60a6d83e5e69b878b6a5eeaa59057df34d52f
Shant Ergenian <shaaant@pm.me>
2024-11-06 09:05:00 -0800

    add PrusaSlicer and FreeCAD configs

--- scripts/setup-home.sh
+++ scripts/setup-home.sh
@@ -12,6 +12,9 @@
 # -----------------------------------------------------------------------------
 
 
+local APPLICATION_SUPPORT="$HOME/Library/Application Support"
+local PREFERENCES="$HOME/Library/Preferences"
+
 function build_directories {
     printf "\nBuilding home directories...\n\n"
     mkdir $HOME/.config
@@ -31,6 +34,7 @@ function build_directories {
 
 function link_dotfiles {
     printf "\nLinking dotfiles...\n\n"
+
     ln -sihv $HOME/.dotfiles/zsh/.zshrc        $HOME/.zshrc
     ln -sihv $HOME/.dotfiles/zsh/.zshenv       $HOME/.zshenv
     ln -sihv $HOME/.dotfiles/git/.gitconfig    $HOME/.gitconfig
@@ -41,14 +45,32 @@ function link_dotfiles {
     ln -sihv $HOME/.dotfiles/kitty             $HOME/.config/kitty
     ln -sihv $HOME/.dotfiles/helix             $HOME/.config/helix
     ln -sihv $HOME/.dotfiles/gitui             $HOME/.config/gitui
+
+    # KiCad
     rm -r                                      $HOME/Library/Preferences/kicad
     ln -sihv $HOME/.dotfiles/kicad/config      $HOME/Library/Preferences/kicad
     ln -sihv $HOME/.dotfiles/kicad/documents   $HOME/Documents/KiCad
+
+    # PrusaSlicer
+    ln -sihv $HOME/.dotfiles/prusa/PrusaSlicer.ini  $APPLICATION_SUPPORT/PrusaSlicer/PrusaSlicer.ini
+    ln -sihv $HOME/.dotfiles/prusa/filament         $APPLICATION_SUPPORT/PrusaSlicer/filament
+    ln -sihv $HOME/.dotfiles/prusa/physical_printer $APPLICATION_SUPPORT/PrusaSlicer/physical_printer
+    ln -sihv $HOME/.dotfiles/prusa/print            $APPLICATION_SUPPORT/PrusaSlicer/print
+    ln -sihv $HOME/.dotfiles/prusa/printer          $APPLICATION_SUPPORT/PrusaSlicer/printer
+    ln -sihv $HOME/.dotfiles/prusa/shapes           $APPLICATION_SUPPORT/PrusaSlicer/shapes
+
+    # FreeCAD
+    ln -sihv $HOME/.dotfiles/freecad/preference_packs/ShantConfig $APPLICATION_SUPPORT/FreeCAD/SavedPreferencePacks/ShantConfig
+    ln -sihv $HOME/.dotfiles/freecad/preference_packs/package.xml $APPLICATION_SUPPORT/FreeCAD/SavedPreferencePacks/package.xml
+    ln -sihv $HOME/.dotfiles/freecad/macros                       $APPLICATION_SUPPORT/FreeCAD/Macro
+    ln -sihv $HOME/.dotfiles/freecad/materials                    $APPLICATION_SUPPORT/FreeCAD/Material
+    ln -sihv $HOME/.dotfiles/freecad/system.cfg                   $PREFERENCES/FreeCAD/system.cfg
+    ln -sihv $HOME/.dotfiles/freecad/user.cfg                     $PREFERENCES/FreeCAD/user.cfg
 }
 
 
 function main {
-    build_directories
+    # build_directories
     link_dotfiles
 }
 
